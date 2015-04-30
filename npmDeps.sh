0ff452f9f97a6f667a2f459b6c64c193f936571f
Jake Grajewski <jake.grajewski@multiscalehn.com>
2015-04-30 13:13:11 -0700

    small update to shell scripts

--- npmDeps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm i -g typescript || sudo npm i -g typescript &&
-
-npm i -g grunt-cli || sudo npm i -g grunt-cli &&
-
-npm i -g tsd@next || sudo npm i -g tsd@next
-
-npm i -g bower || sudo npm i -g bower
